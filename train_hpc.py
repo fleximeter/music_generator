095fcbd21f8fe589ebe2e35bbdb415a549751415
Jeff Martin <jeffreymartin@outlook.com>
2024-05-29 19:46:56 -0500

    more changes for the hpc trainer

--- train_hpc.py
+++ train_hpc.py
@@ -19,7 +19,7 @@ from torch.utils.data import DataLoader
 
 
 def train_sequences(model, dataloader, loss_fns, loss_weights, optimizer, num_epochs, status_interval, 
-                    save_interval, model_metadata, log_file, device="cpu") -> None:
+                    save_interval, model_metadata, device="cpu") -> None:
     """
     Trains the model. This training function expects a DataLoader which will feed it batches
     of sequences in randomized order. The DataLoader takes care of serving up labels as well.
@@ -35,7 +35,6 @@ def train_sequences(model, dataloader, loss_fns, loss_weights, optimizer, num_ep
     :param status_interval: How often (in epochs) to print an update message
     :param save_interval: Save to disk every N epochs
     :param model_metadata: The model metadata
-    :param log_file: The log file to write the model status to
     :param device: The device that is being used for training
     """
 
@@ -94,8 +93,7 @@ def train_sequences(model, dataloader, loss_fns, loss_weights, optimizer, num_ep
         
         # Output status
         if status_interval is not None and epoch % status_interval == status_interval - 1:
-            with open(log_file, "a") as log:
-                log.write(status_message)
+            print(status_message)
 
         # Save to disk if it is the right epoch interval
         if epoch % save_interval == save_interval - 1:
@@ -103,20 +101,18 @@ def train_sequences(model, dataloader, loss_fns, loss_weights, optimizer, num_ep
             with open(FILE_NAME, "w") as model_json_file:
                 model_json_file.write(json.dumps(model_metadata))
             torch.save(model.state_dict(), model_metadata["state_dict"])
-            with open(log_file, "a") as log:
-                log.write("Saved to disk\n")
+            print("Saved to disk\n")
 
 
 if __name__ == "__main__":        
-    PATH = "./data/train"                    # The path to the training corpus
-    FILE_NAME = "./data/model15.json"        # The path to the model metadata JSON file
-    NUM_EPOCHS = 100                        # The number of epochs to train
+    PATH = "/Users/jmartin50/music_generator/data/train"                    # The path to the training corpus
+    FILE_NAME = "/Users/jmartin50/music_generator/data/model21.json"        # The path to the model metadata JSON file
+    NUM_EPOCHS = 2000                        # The number of epochs to train
     LEARNING_RATE = 0.001                    # The model learning rate
     NUM_DATALOADER_WORKERS = 8               # The number of workers for the dataloader
     PRINT_UPDATE_INTERVAL = 1                # The epoch interval for printing training status
     MODEL_SAVE_INTERVAL = 10                 # The epoch interval for saving the model
-    JSON_CORPUS = "data/corpus1.json"        # The JSON corpus to use
-    LOG_FILE = "data/output.log"             # The output log
+    JSON_CORPUS = "/Users/jmartin50/music_generator/data/corpus1.json"        # The JSON corpus to use
     
     # The model metadata - save to JSON file
     model_metadata = {
@@ -124,10 +120,10 @@ if __name__ == "__main__":
         "path": FILE_NAME,
         "training_sequence_min_length": 2,
         "training_sequence_max_length": 20,
-        "num_layers": 4,
+        "num_layers": 10,
         "hidden_size": 1024,
-        "batch_size": 200,
-        "state_dict": "./data/music_sequencer_16.pth",
+        "batch_size": 1000,
+        "state_dict": "/Users/jmartin50/music_generator/data/music_sequencer_21.pth",
         "num_features": feature_definitions.NUM_FEATURES,
         "output_sizes": [len(feature_definitions.LETTER_ACCIDENTAL_OCTAVE_ENCODING), len(feature_definitions.QUARTER_LENGTH_ENCODING)],
         "loss": None
@@ -135,18 +131,17 @@ if __name__ == "__main__":
     with open(FILE_NAME, "w") as model_json_file:
         model_json_file.write(json.dumps(model_metadata))
 
-    log = open(LOG_FILE, "w")
-    log.write("Loading dataset...\n")
+    print("Loading dataset...\n")
     sequence_dataset = dataset.MusicXMLDataSet(featurizer.load_json_corpus(JSON_CORPUS), 
                                                model_metadata["training_sequence_min_length"], 
                                                model_metadata["training_sequence_max_length"])
     dataloader = DataLoader(sequence_dataset, model_metadata["batch_size"], True, 
                             collate_fn=dataset.MusicXMLDataSet.collate, num_workers=NUM_DATALOADER_WORKERS)
-    log.write("Dataset loaded.\n")
+    print("Dataset loaded.\n")
 
     # Prefer CUDA if available, otherwise MPS (if on Apple), or CPU as a last-level default
     device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
-    log.write(f"Using device {device}\n")
+    print(f"Using device {device}\n")
     
     # Load and prepare the model. If retraining the model, we will need to load the
     # previous state dictionary so that we aren't training from scratch.
@@ -158,7 +153,6 @@ if __name__ == "__main__":
     loss_weights = loss_weights * loss_weights.numel() / torch.sum(loss_weights)   # normalize the loss weights
 
     # Train the model
-    log.write(f"Training for {NUM_EPOCHS} epochs...\n")
-    log.close()
-    train_sequences(model, dataloader, loss_fn, loss_weights, optimizer, NUM_EPOCHS, PRINT_UPDATE_INTERVAL, MODEL_SAVE_INTERVAL, model_metadata, LOG_FILE, device)
+    print(f"Training for {NUM_EPOCHS} epochs...\n")
+    train_sequences(model, dataloader, loss_fn, loss_weights, optimizer, NUM_EPOCHS, PRINT_UPDATE_INTERVAL, MODEL_SAVE_INTERVAL, model_metadata, device)
     
